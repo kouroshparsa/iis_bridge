1e8a376c9e569d16e0db2b84db9ca33742719c41
kouroshparsa <kouroshtheking@gmail.com>
2014-11-12 00:50:03 -0800

    adding dependencies - 0.3 release

--- setup.py
+++ setup.py
@@ -16,20 +16,19 @@ from setuptools import Command, setup, find_packages
 
 setup(
     name='iis_bridge',
-    version='0.2',
+    version='0.3',
     url='https://github.com/kouroshparsa/iis_bridge',
-    download_url='https://github.com/kouroshparsa/iis_bridge/packages/0.2',
+    download_url='https://github.com/kouroshparsa/iis_bridge/packages/0.3',
     license='GNU',
     author='Kourosh Parsa',
     author_email="kouroshtheking@gmail.com",
     description='an iis management tool for windows',
     long_description=__doc__,
     packages=find_packages(),
+    install_requires = ['Jinja2>=2.7.3'],
     include_package_data=True,
     zip_safe=False,
     platforms='windows',
-    install_requires=[
-    ],
     classifiers=[
         'Environment :: Web Environment',
         'Operating System :: Microsoft :: Windows',
